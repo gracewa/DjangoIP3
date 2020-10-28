7ca9fff28d3e49eca0b6e0e6efc299e4c2e9aa93
gracewa <33077294+gracewa@users.noreply.github.com>
2020-10-28 07:30:53 +0300

    Initial model, styles and templates

--- postings/models.py
+++ postings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+from rest_framework.reverse import reverse as api_reverse
+
+
+# django hosts --> subdomain for reverse
+
+class ProjectPost(models.Model):
+    # pk aka id --> numbers
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, null=True, blank=True)
+    image = models.ImageField(upload_to ='images/')
+    description = models.TextField(max_length=120, null=True, blank=True)
+    link = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+    @property
+    def owner(self):
+        return self.user
+
+    # def get_absolute_url(self):
+    #     return reverse("api-postings:post-rud", kwargs={'pk': self.pk}) '/api/postings/1/'
+
+    def get_api_url(self, request=None):
+        return api_reverse("api-postings:post-rud", kwargs={'pk': self.pk}, request=request)
