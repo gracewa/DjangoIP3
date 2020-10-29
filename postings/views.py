597f3822ed8b5c0c075c108e656fba44891d67ab
gracewa <33077294+gracewa@users.noreply.github.com>
2020-10-29 06:15:35 +0300

    Added form for adding projects

--- postings/views.py
+++ postings/views.py
@@ -3,6 +3,8 @@ from .models import ProjectPost
 
 from .forms import ProjectForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     userid = request.user.id
@@ -11,14 +13,18 @@ def index(request):
     }
     return render(request, 'home.html', context)
 
+@login_required
 def add_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
 
         if form.is_valid():
-            form.save()
+            form_obj = form.save(commit=False)
+            form_obj.user = request.user
+
+            form_obj.save()
             messages.add_message(request, messages.INFO, 'successfully added')
-            return redirect('core:index')
+            return redirect('postings:index')
     else:
         form = ProjectForm()
     return render(request, 'add_project.html', {'form': form, 'messages': messages})
